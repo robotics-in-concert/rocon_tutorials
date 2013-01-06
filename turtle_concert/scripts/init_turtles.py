7340f1888f979f4b38141cc159dde34b266cd382
Daniel Stonier <d.stonier@gmail.com>
2013-01-06 20:34:19 +0900

    copied tutorials in from everywhere else.

---
+++ turtle_concert/scripts/init_turtles.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# License: BSD
+#   https://raw.github.com/robotics-in-concert/rocon_demo/turtle_concert/master/LICENSE
+#
+##############################################################################
+# Imports
+##############################################################################
+
+# Simple script to clear the turtlesim display on startup.
+
+import roslib
+roslib.load_manifest('turtle_concert')
+import rospy
+import turtlesim.srv as turtlesim_srvs
+
+rospy.init_node('clear_turtles')
+rospy.wait_for_service('kill')
+kill_turtle = rospy.ServiceProxy('kill', turtlesim_srvs.Kill)
+response = kill_turtle("turtle1")
+
+# Only because we can't get services publicly exposed yet.
+# The master sync can do it, but am not yet doing it
+rospy.wait_for_service('spawn')
+spawn_turtle = rospy.ServiceProxy('spawn', turtlesim_srvs.Spawn)
+response = spawn_turtle(5.4,6.4,0.0,"turtle_one")
+response = spawn_turtle(5.4,4.4,0.3,"turtle_two")
---
+++ turtle_stroll/app/readme.txt
@@ -0,0 +1,2 @@
+Currently app definitions are in rocon_apps until app manager has a means
+of parsing app list files again.
