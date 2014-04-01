6ac6623a2a2180c31c681321d6cd8054405d18f3
Daniel Stonier <d.stonier@gmail.com>
2014-04-01 18:28:30 +0900

    rocon_service -> concert_service

--- concert_tutorials/turtle_concert/scripts/teleop.py
+++ concert_tutorials/turtle_concert/scripts/teleop.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #       
 # License: BSD
-#   https://raw.github.com/robotics-in-concert/rocon_tutorials/license/LICENSE
+#   https://raw.github.com/robotics-in-concert/concert_services/license/LICENSE
 #
 ##############################################################################
 # About
@@ -23,6 +23,7 @@ import rocon_python_utils
 ##############################################################################
 # Classes
 ##############################################################################
+
 
 class TurtleTeleop:
     '''
--- concert_tutorials/turtle_concert/scripts/turtle_pond.py
+++ concert_tutorials/turtle_concert/scripts/turtle_pond.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #
 # License: BSD
-#   https://raw.github.com/robotics-in-concert/rocon_services/license/LICENSE
+#   https://raw.github.com/robotics-in-concert/concert_services/license/LICENSE
 #
 ##############################################################################
 # About
